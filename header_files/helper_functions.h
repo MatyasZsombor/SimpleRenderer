5184df690379eeb4b5730904ad9d7a1bcbc7f3db
Matyas Zsombor <mzsombi12345@gmail.com>
2023-12-27 15:45:27 +0100

    Added distance.

--- header_files/helper_functions.h
+++ header_files/helper_functions.h
@@ -5,9 +5,10 @@
 #include "all_headers.h"
 #ifndef RENDERER_HELPERFUNCTIONS_H
 #define RENDERER_HELPERFUNCTIONS_H
-double magnitude(Vector v);
+float distance(float x1, float y1, float x2, float y2);
+float magnitude(Vector v);
 void normalize(Vector v);
-double dot_product(Vector vs[], int n);
+float dot_product(Vector vs[], int n);
 Vector add(Vector vs[], int n);
 Vector cross_product(Vector v1, Vector v2);
 bool orthogonality(Vector v1, Vector v2);
