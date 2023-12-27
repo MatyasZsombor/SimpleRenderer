32b681ff36b63b127cecb5163fbdcd1d04a2be45
Matyas Zsombor <mzsombi12345@gmail.com>
2023-12-27 11:05:57 +0100

    Added a header file with all headers. Include only this one. If you need a new header include it in this file.

---
+++ header_files/all_headers.h
@@ -0,0 +1,14 @@
+//
+// Created by poci on 12/27/23.
+//
+
+
+#ifndef RENDERER_ALL_HEADERS_H
+#define RENDERER_ALL_HEADERS_H
+#include "vector.h"
+#include "helper_functions.h"
+#include "cmath"
+#include "player.h"
+#include "library.h"
+#include "iostream"
+#endif //RENDERER_ALL_HEADERS_H
