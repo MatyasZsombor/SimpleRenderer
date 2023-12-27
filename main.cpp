aaa3751bb05f74b97462cbc43190f80b0cbe360e
Matyas Zsombor <mzsombi12345@gmail.com>
2023-12-27 15:46:56 +0100

    Added basic game loop.

--- main.cpp
+++ main.cpp
@@ -1,24 +1,45 @@
-#include <GL/glut.h>
-#include "header_files/vector.h"
-#include "header_files/helper_functions.h"
-#include "iostream"
+#include "header_files/all_headers.h"
+
+Player player = Player();
+
+const int map[] = {
+1,1,1,1,1,1,1,1,
+1,0,0,0,1,0,0,1,
+1,0,0,0,0,0,0,1,
+1,0,0,1,0,0,0,1,
+1,1,0,0,0,0,0,1,
+1,0,1,0,1,0,0,1,
+1,0,0,0,0,0,0,1,
+1,1,1,1,1,1,1,1
+};
 
 void display()
 {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_TRIANGLES);
-    glVertex2f(-0.5f, -0.5f);
-    glVertex2f( 0.0f,  0.5f);
-    glVertex2f( 0.5f, -0.5f);
-    glEnd();
-    glFlush();
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    draw_map2d(map, 8, 8);
+    cast_rays(map, 8, 8);
+    draw_player();
+    glutSwapBuffers();
+}
+
+void init()
+{
+
+    glClearColor(0.3, 0.3, 0.3, 0);
+    gluOrtho2D(0, 1024, 512, 0);
+    player.pos.x = 300;
+    player.pos.y = 300;
 }
 
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
-    glutCreateWindow("OpenGL Test");
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(1024, 512);
+    glutCreateWindow("Simple Ray caster");
+    init();
     glutDisplayFunc(display);
+    glutKeyboardFunc(inputs);
     glutMainLoop();
     return 0;
 }
