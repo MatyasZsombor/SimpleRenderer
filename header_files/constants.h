2716a5e831a33917e5087c579aa92f4d9f1c5681
Matyas Zsombor <mzsombi12345@gmail.com>
2023-12-28 00:31:40 +0100

    Added new constants.

--- header_files/constants.h
+++ header_files/constants.h
@@ -7,4 +7,8 @@
 #define PI M_PIf
 #define FLT_MAX std::numeric_limits<float>::max()
 #define DEGREE (PI / 180)
+#define MAP_HEIGHT 24
+#define MAP_WIDTH  24
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
 #endif //RENDERER_CONSTANTS_H
