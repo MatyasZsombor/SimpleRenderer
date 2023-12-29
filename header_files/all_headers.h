f561717e22e4ac241884b4a30b68ee4c8021cea6
Matyas Zsombor <mzsombi12345@gmail.com>
2023-12-29 22:45:19 +0100

    Added new includes.

--- header_files/all_headers.h
+++ header_files/all_headers.h
@@ -4,11 +4,18 @@
 
 #ifndef RENDERER_ALL_HEADERS_H
 #define RENDERER_ALL_HEADERS_H
+#include <iostream>
+#include <cstdint>
+
+typedef uint32_t u32;
+typedef uint16_t u16;
+typedef uint8_t u8;
+
 #include "SDL2/SDL.h"
 #include "cmath"
 #include "constants.h"
-#include "vector.h"
 #include "RGB.h"
+#include "vector.h"
 #include "player.h"
 #include "state.h"
 #include "helper_functions.h"
