579778b400b623cb5b4131d2f022e1f84b31572b
Roland Fornvald <r.fornvald@HTBLALeonding.onmicrosoft.com>
2023-12-29 12:58:42 +0100

    Added space

--- header_files/library.h
+++ header_files/library.h
@@ -7,6 +7,8 @@ extern Vector cameraPlane;
 extern int map[MAP_WIDTH][MAP_HEIGHT];
 extern double moveSpeed;
 extern double rotSpeed;
+extern double planeX;
+extern double planeY;
 void draw_line(Vector s, Vector e, int w, RGB color);
 void draw_map2d(int map[MAP_WIDTH][MAP_HEIGHT]);
 void draw_player();
